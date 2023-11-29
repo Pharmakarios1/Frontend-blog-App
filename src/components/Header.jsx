2e88668716467f352ed3e3df1303b6d2af5d97b4
Festus1914 <oladiplentyfestus@gmail.com>
2023-11-29 13:27:31 +0100

    Adding files

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,43 +1,99 @@
-import React from 'react'
+import React, {  useState } from 'react'
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { MdKeyboardArrowDown } from "react-icons/md";
 import { images } from '../constants/index'
+import { unstable_renderSubtreeIntoContainer } from 'react-dom';
 
 
 const NavItemsInfo = [
-  {name: "Home"},
-  {name: "Articles"},
-  {name: "Pages"},
-  {name: "Pricing"},
-  {name: "Faq"},
+  {name: "Home", type: "link"},
+  {name: "Articles", type: "link"},
+  {name: "Pages", type: "dropdown", items: ["About us", "Contact us"] },
+  {name: "Pricing", type: "link"},
+  {name: "Faq", type: "link"},
 ]
 
-const NavItem = ({name})=>{
+const NavItem = ({ item })=>{
+const [dropdown, setDropdown] = useState(false)
+
+const toggleDropdownHandler = () => {
+  setDropdown((curState) => {
+    return !curState;
+  });
+};
+
   return( 
     <li className='relative group'>
+      {item.type === "link" ?  ( 
+      <>
       <a href="/" className='px-4 py-2'>
-        {name}
+        {item.name}
+        
         </a>
-        <span className='text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100'>
+        <span className=' cursor-pointer text-blue-500 absolute transition-all duration-500 font-bold right-0 top-0 group-hover:right-[90%] opacity-0 group-hover:opacity-100'>
           /
           </span>
-          </li>
-          );
-        }
+          </>
+          ) : ( 
+          <div className='flex flex-col item-center'>
+          <button className='px-4 py-0 flex gap-x-2 items-center' onClick={toggleDropdownHandler}>
+            <span>{item.name}</span>
+            <MdKeyboardArrowDown />
+            </button>
+        <div className={`${dropdown ? "block" : "hidden"} lg:hidden transition-all duration-500 pt-4 lg:absolute lg:bottom-0 lg:right-0 lg:transform lg:translate-y-full lg:group-hover:block w-max`}>
+          <ul className=' bg-dark-soft lg:bg-transparent text-center flex flex-col shadow-lg rounded-lg overflow-hidden'>
+            {item.items.map((page)=> (
+            
+              <a 
+              href="/" 
+              className='hover:bg-dark-hard hover:text-white px-4 py-2 text-white lg:text-dark-soft'>
+                {page}
+                </a>
+            ))}
+          </ul>
+        </div>
+        </div>
+        )}
+        </li>
+        );
+      };
 
 const Header = () => {
+  const [navIsVisible, setNavIsInvisible] = useState(false);
+
+  const navVisibilityHandler = () => {
+    setNavIsInvisible((curState) => {
+      return !curState;
+    });
+  };
+  
+  
+  
   return (
   <section>
     <header className='container mx-auto px-5 flex justify-between py-4 items-center'>
         <div>
-            <img src={images.Logo} alt="logo" />
+            <img className='w-16' src={images.Logo} alt="logo" />
         </div>
-        <div className='flex gap-x-9 items-center'>
-          <ul className='flex gap-x-5 font-semibold'>
+        <div className='lg:hidden z-50'>
+          {navIsVisible ? ( 
+          <AiOutlineClose
+          className='w-6 h-6' 
+          onClick={navVisibilityHandler} 
+          /> 
+          ): ( 
+          <AiOutlineMenu className='w-6 h-6' onClick={navVisibilityHandler} />
+          )}
+        </div>
+        <div 
+        className={`${navIsVisible ? "right-0" : "-right-full"} transition-all duration-300  mt-[56] lg:mt-0 bg-dark-hard lg:bg-transparent z-[49] flex flex-col w-full lg:w-auto lg:flex-row justify-center lg:justify-end fixed top-0 bottom-0  lg:static gap-x-9 items-center`}>
+          <ul className='text-white item-center gap-y-5 lg:text-dark-soft  flex flex-col lg:flex-row gap-x-5 font-semibold'>
             {NavItemsInfo.map((item) => (
-              <NavItem key={item} name={item.name} />
+              <NavItem key={item} item={item} />
 
             ))}
           </ul>
-          <button className='border-2  border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300'>Sign in
+          <button className='mt-5 lg:mt-0 border-2  border-blue-500 px-6 py-2 rounded-full text-blue-500 font-semibold hover:bg-blue-500 hover:text-white transition-all duration-300'>Sign in
           </button>
         </div>
     </header>
