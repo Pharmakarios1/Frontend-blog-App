7003edd77520d9e90bfe1e856486c90d1f1dcc58
Festus1914 <oladiplentyfestus@gmail.com>
2023-12-01 21:26:35 +0100

    Adding cards files

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,13 @@
 import Logo from '../assets/logo.png'
 import HeroImage from '../assets/HeroImage.png'
+import Post1Image from '../assets/post1.jpg'
+import PostProfileImage from '../assets/post-profile.jpg'
 
 const images = {
     Logo,
     HeroImage,
+    Post1Image,
+    PostProfileImage,
     
     
 };
