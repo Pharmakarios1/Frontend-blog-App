049e45728e09c9e0377a9501c44f30d734796f05
Festus1914 <oladiplentyfestus@gmail.com>
2023-12-04 10:02:53 +0100

    Pushing some changed files

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,19 +4,19 @@ import Logo from "../assets/logo.png";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#0D2436] font-roboto mx-auto min-h-max p-10">
-      <section className="flex justify-between align-center text-[#959EAD]">
-        <div>
+    <section className="bg-dark-hard">
+    <footer className="container mx-auto grid grid-cols-10 px-5 py-10 gap-y-10 gap-x-5 md:pt-20 md:grid-cols-12 lg:grid-cols-10 lg:gap-x-10">
+       <div>
           <div>
             <img className="w-16" src={Logo} alt="logo" />
           </div>
-          <div className="mt-8">
-          <p> Build a modern and creative <br /> website with us. </p>
-          </div>
+          <section className="col-span-5 md:col-span-4 lg:col-span-2 text-dark-light font-bold md:text-lg">
+          <p className="text-sm text-dark-light text-center mt-4 md:text-left md:text-base lg:text-sm"> Build a modern and creative website with us.</p>
+          </section>
         </div>
-        <div>
-          <ul>
-            <li>Product</li>
+        <div className="col-span-5 md:col-span-4 lg:col-span-2 ml-auto">
+        <h3 className="text-dark-light font-bold md:text-lg">Product</h3>
+          <ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base">
             <li>Landingpage</li>
             <li>Features</li>
             <li>Documentation</li>
@@ -24,9 +24,9 @@ const Footer = () => {
             <li>Pricing</li>
           </ul>
         </div>
-        <div>
-          <ul>
-            <li>Services</li>
+        <div className="col-span-5 md:col-span-4 lg:col-span-2">
+        <h3 className="text-dark-light font-bold md:text-lg">Services</h3>
+          <ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base">
             <li>Documentation</li>
             <li>Design</li>
             <li>Frontend Development</li>
@@ -34,28 +34,27 @@ const Footer = () => {
             <li>Dev-ops</li>
           </ul>
         </div>
-        <div><ul>
-          <li>Company</li>
+        <div className="col-span-5 md:col-span-4 md:col-start-5 lg:col-start-auto lg:col-span-2">
+        <h3 className="text-dark-light font-bold md:text-lg">Company</h3>
+          <ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base">
           <li>About</li>
           <li>Terms</li>
           <li>Privacy Policy</li>
           <li>Careers</li>
         </ul>
         </div>
-        <div>
-          <ul>
-            <li>More</li>
+        <div className="col-span-5 md:col-span-4 lg:col-span-2">
+        <h3 className="text-dark-light font-bold md:text-lg">More</h3>
+          <ul className="text-[#959EAD] text-sm mt-5 space-y-4 md:text-base" >
             <li>Documentation</li>
             <li>License</li>
             <li>Changelog</li>
             <li></li>
           </ul>
         </div>
-      </section>
-      <section className="flex justify-center align-center h-10">
-        <p className="mt-8 bg-white h-6">Copyright © 2023. Blog App.</p>
-      </section>
-    </footer>
+        <p className="font-bold italic text-dark-light">Copyright © 2023. Blog App.</p>
+        </footer>
+    </section>
   );
 };
 
