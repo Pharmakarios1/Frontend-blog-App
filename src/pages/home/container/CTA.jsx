96a6a3e26043bb4eb1787923c8061506eb972607
Festus1914 <oladiplentyfestus@gmail.com>
2023-12-04 09:03:17 +0100

    Adding some changes

--- src/pages/home/container/CTA.jsx
+++ src/pages/home/container/CTA.jsx
@@ -24,7 +24,7 @@ const CTA = () => {
         <div className="container grid grid-cols-12 mx-auto py-10 md:pb-20 lg:place-items-center">
           <div className="col-span-12 lg:col-span-6">
             <h2 className="text-white font-roboto font-bold text-2xl md:text-4xl md:text-center md:leading-normal lg:text-left">
-              Get our stories delivered From us to your inbox weekly.
+            Manage your blogs accurately with us
             </h2>
             <div className="w-full max-w-[494px] mt-12 space-y-3 mx-auto md:space-y-0 md:flex md:items-center md:space-x-2 lg:mx-0">
               <input
@@ -56,10 +56,10 @@ const CTA = () => {
                 />
                 <div className="p-5">
                   <h2 className="font-roboto font-bold text-xl text-dark-soft md:text-2xl lg:text-[28px]">
-                    Future of Work
+                  The best aticles every week
                   </h2>
                   <p className="text-dark-light mt-3 text-sm md:text-lg">
-                    Majority of peole will work in jobs that don’t exist today.
+                  Our insurance plans offers are priced the same everywhere else.
                   </p>
                 </div>
               </div>
